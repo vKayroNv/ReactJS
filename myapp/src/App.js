2fe419f44878ab8cf0564c00890fc413b6d7e512
Kayron <kayron.eldrag@gmail.com>
2022-08-15 21:54:18 +0300

    Добавлено: - стили - форма ввода - список сообщений - автоответчик

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
 
 function App() {
+
+  const [messagesList, setMessagesList] = useState([{}]);
+  const botName = 'bot';
+
+  const newMessage = (input) => {
+    setMessagesList(messagesList => { return [...messagesList,input]})
+  }
+
+  useEffect(()=>{
+    const lastMessage = messagesList[messagesList.length - 1];
+
+    if (lastMessage.author && lastMessage.author !== botName) {
+        setTimeout(()=>setMessagesList(messages => {
+          return [...messages, {author: botName, messageText: `Здравствуйте, ${lastMessage.author}!`}]
+      }), 2000)
+    }
+  },[messagesList])
+
+  const MessageInput = props => {
+    const [messageText, setMessageText] = useState('');
+    const [author, setAuthor] = useState('');
+
+    return (
+      <div className='input-area'>
+        <input type="text" autoFocus placeholder="Имя" value={author} onChange={event => setAuthor(event.target.value)}/>
+        <input type="text" placeholder="Сообщение" value={messageText} onChange={event => setMessageText(event.target.value)}/>
+        <button onClick={() => { if (author && messageText) props.onChangeMessage({ author: author, messageText: messageText }) }}>Отправить</button>
+      </div>
+    );
+  }
+
+  function Message({author, messageText}) {
+    if (author && messageText)
+      return (
+        <p>{author}: {messageText}</p>
+      )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='messages-area'>
+        {messagesList.map(({author, messageText}, index) => <Message key={index} author={author} messageText={messageText}/>)}
+      </div>
+      <MessageInput onChangeMessage={newMessage}/>
+    </>
   );
 }
 

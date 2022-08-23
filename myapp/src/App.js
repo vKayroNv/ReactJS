bf8ea1c2885a549df8f76057ecaea29178f5d33a
Kayron <kayron.eldrag@gmail.com>
2022-08-23 19:11:16 +0300

    Добавлена возможность изменения имени на странице профиля

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -5,16 +5,24 @@ import Profile from './components/Profile';
 import Chats from './components/Chats';
 import Home from './components/Home';
 import Header from './components/Header';
+import { useState } from 'react';
   
 
 function App() {
+
+  const [username, setUsername] = useState('testuser');
+
+  const changeUsername = input => {
+    setUsername(input);
+  }
+
   return (
     <>
       <BrowserRouter>
         <Header/>
         <Routes>
           <Route path="/" element={<Home />}/>
-          <Route path="/profile" element={<Profile />}/>
+          <Route path="/profile" element={<Profile username={username} onChangeUsername={changeUsername} />}/>
           <Route path="/chats" element={<Chats />}/>
         </Routes>
       </BrowserRouter>
--- myapp/src/components/Profile.js
+++ myapp/src/components/Profile.js
@@ -1,7 +1,24 @@
-export default function Profile(){
+import {useState} from 'react';
+import { Button, TextField, Container } from '@mui/material';
+
+export default function Profile(props){
+
+  const [username, setUsername] = useState(props.username);
+
+  const changeUsername = input => {
+    setUsername(input);
+  }
+
   return(
-    <div>
-      Страница профиля
-    </div>
+    <>
+      <br/>
+      <Container>
+        <TextField autoFocus fullWidth label="Ваше имя" variant="filled" value={username} onChange={event => changeUsername(event.target.value)}/>
+        <Button fullWidth variant="contained" onClick={() => { 
+          props.onChangeUsername(username);
+          alert('Имя изменено');
+        }}>Изменить</Button>
+      </Container>
+    </>
   );
 }

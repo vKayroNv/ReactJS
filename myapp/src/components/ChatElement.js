83a2b341971a1470fa68a4afccbaca21bdc2a85d
Kayron <kayron.eldrag@gmail.com>
2022-08-25 18:23:46 +0300

    Исправление ошибки fullWidth в ListItemButton

--- myapp/src/components/ChatElement.js
+++ myapp/src/components/ChatElement.js
@@ -5,7 +5,7 @@ import ClearIcon from '@mui/icons-material/Clear';
 export default function ChatElement(props) {
   return(
     <ListItem disablePadding>
-      <ListItemButton fullWidth variant="outlined" color="inherit" component={Link} to={`/chats/${props.chatId}`}>
+      <ListItemButton variant="outlined" color="inherit" component={Link} to={`/chats/${props.chatId}`}>
         {props.name}
       </ListItemButton>
       {/* <Button variant="contained" color="error" style={{maxWidth: '36.5px', minWidth: '36.5px'}}>
